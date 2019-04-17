36e22091465d24f565b2ab4f52a3666bfbfebce5
Warut Surapat <wsurapat@gilt.jp>
2019-04-17 18:33:38 +0900

    Try templating that copy source from src folder.

--- templating/templating.ex
+++ templating/templating.ex
@@ -6,4 +6,13 @@ defmodule Templating do
     File.write("output.rb", output)
   end
 
+  def gen(name) do
+    System.cmd("mkdir", ["ruby"])
+    {:ok, text} = File.read("template.eex")
+    output = EEx.eval_string(text, name: name)
+    File.write("ruby/output.rb", output)
+    System.cmd("cp", ["src/Gemfile", "ruby"])
+    System.cmd("bundle", ["install"], cd: "ruby")
+  end
+
 end
