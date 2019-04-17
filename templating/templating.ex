7c53f9cb6c6a3e5fe13477d5596a4cbeda677df7
Warut Surapat <wsurapat@gilt.jp>
2019-04-17 18:44:45 +0900

    Add draft project generator steps idea.

--- templating/templating.ex
+++ templating/templating.ex
@@ -15,4 +15,19 @@ defmodule Templating do
     System.cmd("bundle", ["install"], cd: "ruby")
   end
 
+  # def gen_ruby_main_file do
+
+  # end
+
+  # def gen_ruby do
+  #   create_ruby_folder
+  #   |> gen_ruby_main_file
+  #   |> copy_gem_file
+  #   |> run_bundle_install
+  # end
+
+  # def gen_elixir do
+  #   run_mix_new_project(name)
+  # end
+
 end
