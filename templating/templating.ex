7c9171f61a6dde360b2ef8982320fc7803daef7b
Warut Surapat <s.warut@gmail.com>
2019-04-18 00:45:06 +0900

    Get basic template working.

--- templating/templating.ex
+++ templating/templating.ex
@@ -6,18 +6,31 @@ defmodule Templating do
     File.write("output.rb", output)
   end
 
+
   def gen(name) do
     System.cmd("mkdir", ["ruby"])
-    {:ok, text} = File.read("template.eex")
-    output = EEx.eval_string(text, name: name)
-    File.write("ruby/output.rb", output)
+    System.cmd("mkdir", ["spec"], cd: "ruby")
     System.cmd("cp", ["src/Gemfile", "ruby"])
     System.cmd("bundle", ["install"], cd: "ruby")
+    gen_ruby_main_file(name)
+    gen_ruby_spec_file(name)
   end
 
-  # def gen_ruby_main_file do
+  def gen_ruby_main_file(name) do
+    output = get_ruby_output("src/ruby_base.eex", name)
+    File.write("ruby/#{name}.rb", output)
+  end
 
-  # end
+  def gen_ruby_spec_file(name) do
+    output = get_ruby_output("src/ruby_spec_base.eex", name)
+    File.write("ruby/spec/#{name}_spec.rb", output)
+  end
+
+  def get_ruby_output(file_name, name) do
+    {:ok, text} = File.read(file_name)
+    class_name = String.split(name, "_") |> Enum.map(fn(t) -> String.capitalize(t) end) |>Enum.join
+    output = EEx.eval_string(text, name: name, class_name: class_name)
+  end
 
   # def gen_ruby do
   #   create_ruby_folder
